282dd27d6aaec43e00979fcb824c1129593f9c35
dell <dell@DESKTOP-KTJNK95>
2021-09-14 20:29:02 +0530

    Add project files.

---
+++ enhancedlp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enhancedlp
+{
+    class demo
+    {
+        int sum = 0;
+        public int add(int[] list)
+        {
+            foreach(int x in list)
+            {
+                sum = sum + x;   
+            }
+            return sum;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lst = { 19, 33, 45, 54, 23, 78, 12 };
+            demo d1 = new demo();
+            int res = d1.add(lst);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
